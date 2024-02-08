1d0d994f6234a70a8341924ca32a5ee00ae488d7
Parinya Punpongsanon <parinya@mail.saitama-u.ac.jp>
2024-02-08 14:54:23 +0900

    Update 09_polynomial_regression2.py

--- 09_statistics/09_polynomial_regression2.py
+++ 09_statistics/09_polynomial_regression2.py
@@ -1,4 +1,8 @@
 """
+演習課題 6 : 様々な回帰モデル
+"""
+
+"""
     多項式回帰による予測
     
     注意：このコードの実行前にに以下のコードを実行してください．
@@ -19,12 +23,13 @@ import japanize_matplotlib # 日本語表示用モジュール
 
 from sklearn.preprocessing import PolynomialFeatures # 回帰分析する入力を定める関数
 from sklearn.linear_model import LinearRegression # 回帰分析する関数
+from sklearn.linear_model import Ridge # 回帰分析する関数
 from sklearn.metrics import r2_score # R2スコア(どれだけモデルがデータにマッチしているか)算出
 
 """
 データの読みこみ
 """
-input_file = 'bukka.csv'  # 読み込むファイル名
+input_file = '/content/drive/MyDrive/Colab Notebooks/AL1_Introduction_to_Programming/bukka.csv'  # 読み込むファイル名
 input_data = pd.read_csv(input_file)
 input_data = np.array(input_data)
 
@@ -45,33 +50,34 @@ years_std = scaler_year.fit_transform(years) # 標準化した年次
 bukka_std = scaler_bukka.fit_transform(bukka) # 標準化した消費者物価指数
 
 """
-多項式回帰：degreeで○次関数を決める．
+#####################################
+線形回帰：degreeで「1」次関数を決める．
+#####################################
 """
 # 年次を入力として消費者物価指数を予測する．
-polynomial_features= PolynomialFeatures(degree=2) # 2次関数(直線)を使用
-input_ploy = polynomial_features.fit_transform(years_std) # 入力を定める
+linear_features= PolynomialFeatures(degree=1) # 2次関数(直線)を使用
+input_linear = linear_features.fit_transform(years_std) # 入力を定める
 
 # 線形モデルを当てはめる
-model = LinearRegression()  # 回帰分析の準備
-model.fit(input_ploy, bukka_std)    # 回帰分析の実施
-bukka_pred = model.predict(input_ploy)# 回帰分析の結果を出力
+model_linear = LinearRegression()  # 回帰分析の準備
+model_linear.fit(input_linear, bukka_std)    # 回帰分析の実施
+bukka_linear_pred = model_linear.predict(input_linear)# 回帰分析の結果を出力
 
 # 標準化したデータを元に戻す
-bukka_pred = scaler_bukka.inverse_transform(bukka_pred)
+bukka_linear_pred = scaler_bukka.inverse_transform(bukka_linear_pred)
 
 # グラフにプロットする
 plt.plot(years, bukka)  # 実データ
-plt.plot(years, bukka_pred) # 回帰結果
+plt.plot(years, bukka_linear_pred) # 回帰結果
 plt.xlabel('年次')
 plt.ylabel('消費者物価指数')
 
 # R2スコア
-r2 = r2_score(bukka, bukka_pred)
-print('R2 Score : {0:.2}'.format(r2))
+r2 = r2_score(bukka, bukka_linear_pred)
+print('線形回帰')
+print(' R2 Score : {0:.2}'.format(r2))
 
-"""
-2023年，2030年の予測
-"""
+# 2023年，2030年の予測
 years_future = np.array([2023,2030]) # 予測したい年のデータ
 years_future = years_future[:,np.newaxis] # おまじない
 
@@ -79,11 +85,56 @@ years_future = years_future[:,np.newaxis] # おまじない
 years_future = scaler_year.transform(years_future)
 
 # 予測
-input_ploy = polynomial_features.fit_transform(years_future) # 入力を定める
-bukka_future = model.predict(input_ploy) # 予測の結果
+input_linear = linear_features.fit_transform(years_future) # 入力を定める
+bukka_linear_future = model_linear.predict(input_linear) # 予測の結果
 
 # 標準化したデータを元に戻す
-bukka_future = scaler_bukka.inverse_transform(bukka_future)
+bukka_linear_future = scaler_bukka.inverse_transform(bukka_linear_future)
 
-print(' 2023年の消費者物価指数：{0:.1f} '.format(bukka_future[0,0]))
-print(' 2030年の消費者物価指数：{0:.1f} '.format(bukka_future[1,0]))
+print(' 2023年の消費者物価指数：{0:.1f} '.format(bukka_linear_future[0,0]))
+print(' 2030年の消費者物価指数：{0:.1f} '.format(bukka_linear_future[1,0]))
+
+"""
+#####################################
+多項式回帰：degreeで「<2」次関数を決める．
+#####################################
+"""
+# 年次を入力として消費者物価指数を予測する．
+polynomial_features= PolynomialFeatures(degree=2) # 2次関数(直線)を使用
+input_poly = polynomial_features.fit_transform(years_std) # 入力を定める
+
+# 線形モデルを当てはめる
+model_poly = LinearRegression()  # 回帰分析の準備
+model_poly.fit(input_poly, bukka_std)    # 回帰分析の実施
+bukka_poly_pred = model_poly.predict(input_poly)# 回帰分析の結果を出力
+
+# 標準化したデータを元に戻す
+bukka_poly_pred = scaler_bukka.inverse_transform(bukka_poly_pred)
+
+# グラフにプロットする
+plt.plot(years, bukka)  # 実データ
+plt.plot(years, bukka_poly_pred) # 回帰結果
+plt.xlabel('年次')
+plt.ylabel('消費者物価指数')
+
+# R2スコア
+r2 = r2_score(bukka, bukka_poly_pred)
+print('多項式回帰')
+print(' R2 Score : {0:.2}'.format(r2))
+
+# 2023年，2030年の予測
+years_future = np.array([2023,2030]) # 予測したい年のデータ
+years_future = years_future[:,np.newaxis] # おまじない
+
+# 標準化
+years_future = scaler_year.transform(years_future)
+
+# 予測
+input_poly = polynomial_features.fit_transform(years_future) # 入力を定める
+bukka_poly_future = model_poly.predict(input_poly) # 予測の結果
+
+# 標準化したデータを元に戻す
+bukka_poly_future = scaler_bukka.inverse_transform(bukka_poly_future)
+
+print(' 2023年の消費者物価指数：{0:.1f} '.format(bukka_poly_future[0,0]))
+print(' 2030年の消費者物価指数：{0:.1f} '.format(bukka_poly_future[1,0]))
