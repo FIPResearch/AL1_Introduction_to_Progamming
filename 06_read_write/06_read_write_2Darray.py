82b1c30ab7f49b4246956221ea435194315bba21
Parinya Punpongsanon <parinya@mail.saitama-u.ac.jp>
2024-02-08 13:59:06 +0900

    Update 06_read_write_2Darray.py

--- 06_read_write/06_read_write_2Darray.py
+++ 06_read_write/06_read_write_2Darray.py
@@ -10,7 +10,7 @@ input_file = 'iris.csv'	# 読み込むファイル名
 input_data = pd.read_csv(input_file)
 input_data = np.array(input_data)
 
-# 「variety」以外の要素を配列output_dataに入れる．．
+# 「variety」以外の要素を配列output_dataに入れる
 output_data = []
 for row in input_data:	# １行ずつ取り出して row に入れる．
 	output_data.append(row[0:4])
